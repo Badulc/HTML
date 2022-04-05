82764a8e4dfe7b32571adc67ad163374c135838c
badulchowdhury <badulchowdhury@protonmail.com>
2022-04-05 11:03:54 +0100

    Js example and task 1&3

---
+++ JavaScript/example Js/script.js
@@ -0,0 +1,22 @@
+// USE STRICT and  CONSOLE
+
+/*
+use strict:
+forces us to write 'secure' JavaScript
+stops us from using protected words, accidentally making global variables etc...
+*/
+
+`use strict`
+
+console.log(`hello world`);
+
+console.info(`this is info`);
+
+console.warn(`this is a warning`);
+
+console.error(`this is an error nessage`);
+
+//styling the console
+
+console.log(`%c` + `red text` + `%c` + `blue text`, `color: red;`, `color: blue;`);
+
---
+++ JavaScript/task1&3/script.js
@@ -0,0 +1,13 @@
+`use strict`
+
+let fName = "Badul";
+let lName = "Chowdhury";
+let loc = "London";
+let stSign = "Astrological";
+
+console.log(fName);
+console.info(lName);
+console.warn(loc);
+console.error(stSign);
+
+console.log("This is now %c the end of the exercises.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
